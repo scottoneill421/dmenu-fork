8a4ea052b9d72f31ca525cb325f53a52d39c5c60
scottoneill421 <scott.oneill421@protonmail.com>
2021-10-04 14:38:38 +1100

    repo for custom dmenu

--- config.h
+++ config.h
@@ -1,6 +1,10 @@
 /* See LICENSE file for copyright and license details. */
 /* Default settings; can be overriden by command line. */
 
+int border_width = 3;
+int columns = 4;
+int menu_width = 350;
+
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
@@ -9,19 +13,15 @@ static const char *fonts[] = {
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+	[SchemeNorm] = { "#00ff00", "#000000" },
+	[SchemeSel] = { "#75ff75", "#007500" },
+	[SchemeOut] = { "#ff0000", "#ff0000" },
 };
-/* -l and -g options; controls number of lines and columns in grid if > 0 */
+/* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
-static unsigned int columns    = 0;
 
 /*
  * Characters not considered part of a word while deleting words
  * for example: " /?\"&[]"
  */
 static const char worddelimiters[] = " ";
-
-/* Size of the window border */
-static const unsigned int border_width = 5;
